2db413e804fef0de6fac8241ac27fbf10f85ceab
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-06-30 20:15:56 -0300

    Project completed.

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,14 @@
 package model.dao;
 
-import model.dao.impl.SellerDaoJDBC;
+import db.DB;
 
 public class DaoFactory {
-	
-	public static  SellerDao createSellerdao() {
-		return new SellerDaoJDBC();
-	}
 
+	public static SellerDao createSellerDao() {
+		return new impl.SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new impl.DepartmentDaoJDBC(DB.getConnection());
+	}
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -2,7 +2,6 @@ package model.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
 
 public class Seller implements Serializable {
 
@@ -13,11 +12,10 @@ public class Seller implements Serializable {
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
-
+	
 	private Department department;
-
+	
 	public Seller() {
-
 	}
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
@@ -79,7 +77,10 @@ public class Seller implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -91,7 +92,12 @@ public class Seller implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -99,5 +105,4 @@ public class Seller implements Serializable {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-
 }
